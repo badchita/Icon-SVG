564a30e061fbf47a2c994d9d1d55b01389fdd848
badchita <47920377+badchita@users.noreply.github.com>
2020-11-21 16:57:55 +0800

    update

--- index.php
+++ index.php
@@ -19,9 +19,7 @@
 <body>
     <div class="container">
         <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-            <!-- Created with Method Draw - http://github.com/duopixel/Method-Draw/ -->
             <g>
-                <!-- 1st Railway -->
                 <path
                     d="m50,150c0,-55.24862 44.75138,-100 100,-100c55.24862,0 100,44.75138 100,100c0,55.24862 -44.75138,100 -100,100c-55.24862,0 -100,-44.75138 -100,-100z"
                     stroke-opacity="null" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="2,8"
@@ -29,7 +27,6 @@
                     <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 150 150"
                         to="360 150 150" dur="8s" repeatCount="indefinite" />
                 </path>
-                <!-- 2nd Railway -->
                 <path id="svg_2"
                     d="m80,150c0,-38.67403 31.32597,-70 70,-70c38.67403,0 70,31.32597 70,70c0,38.67403 -31.32597,70 -70,70c-38.67403,0 -70,-31.32597 -70,-70z"
                     stroke-opacity="null" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="2,8"
@@ -37,7 +34,6 @@
                     <animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 150 150"
                         to="360 150 150" dur="8s" repeatCount="indefinite" />
                 </path>
-                <!-- 3rd Railway -->
                 <path id="svg_3"
                     d="m110,150c0,-22.09945 17.90055,-40 40,-40c22.09945,0 40,17.90055 40,40c0,22.09945 -17.90055,40 -40,40c-22.09945,0 -40,-17.90055 -40,-40z"
                     stroke-opacity="null" stroke-linecap="null" stroke-linejoin="null" stroke-dasharray="2,8"
